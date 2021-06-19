c101ef19191de181ccbfde55ffb19aaa7e4b2d8e
Sharlikran <danielhmpdx16@gmail.com>
2021-06-19 09:11:13 -0700

    Version: 23

--- Settings.lua
+++ Settings.lua
@@ -305,6 +305,7 @@ function LoreBooks:CreateSettings()
 				end,
 			default = immersiveChoices[defaults.immersiveMode],
 		},
+		--[[
 		{
 			type = "checkbox",
 			name = GetString(LBOOKS_UNLOCK_EIDETIC),
@@ -346,6 +347,7 @@ function LoreBooks:CreateSettings()
 			default = defaults.shareData,
 			disabled = GetWorldName() ~= "EU Megaserver" or not c.SUPPORTED_LANG[lang],
 		},
+		--]]
 	}
 	LAM:RegisterOptionControls(c.ADDON_PANEL, optionsTable)
 
