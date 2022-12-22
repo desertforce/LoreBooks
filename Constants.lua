c7f6a382f843a45780763a3860c8c7486d61c63b
Sharlikran <danielhmpdx16@gmail.com>
2022-12-22 04:43:59 -0800

    Version: 54

--- Constants.lua
+++ Constants.lua
@@ -6,7 +6,7 @@ _G["LoreBooks_Internal"] = internal
 --Local constants -------------------------------------------------------------
 internal.ADDON_NAME = "LoreBooks"
 internal.ADDON_AUTHOR = "Garkin, Ayantir, Kyoma, Sharlikran"
-internal.ADDON_VERSION = "53"
+internal.ADDON_VERSION = "54"
 internal.ADDON_WEBSITE = "http://www.esoui.com/downloads/info288-LoreBooks.html"
 internal.ADDON_PANEL = "LoreBooksPanel"
 internal.SAVEDVARIABLES_VERSION = 3
