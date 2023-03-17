6edbde5b1ff167d7767ea22a8bd810850d335244
Sharlikran <danielhmpdx16@gmail.com>
2023-03-16 17:53:25 -0700

    Version: 68

--- LibLoreLibrary/Data_101037.lua
+++ LibLoreLibrary/Data_101037.lua
@@ -2,7 +2,7 @@ LibLoreLibrary = LibLoreLibrary or {}
 
 local lib = LibLoreLibrary
 
-lib.data[101036] = {
+lib.data[101037] = {
   [1] = {
     [1] = {
       ["g"] = "/esoui/art/treeicons/gamepad/gp_lorelibrary_categoryicon_places.dds",
